a01fce1c6ecbfcff385bb904f633b28612b69e4b
Boku7-glitch <sababokuchava2009@gmail.com>
2025-04-16 23:53:59 +0400

    Updated Project

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Web I Final Project' });
+});
+
+router.get('/dashboard', (req, res) => {
+  if (!req.session.user) return res.redirect('/login');
+  res.send(`Welcome, ${req.session.user.email}`);
 });
 
 module.exports = router;
